d7b22dcdb574c1d9a3c5e380310b4576acf019e2
amesemyta1 <amesemyta@gmail.com>
2026-05-26 17:13:08 +0300

    feat(catalog): register models in django admin and configure catalog app

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from catalog.models import AlcoholType, Taster, Beverage, Snack, Flavor
+
+
+@admin.register(Taster)
+class TasterAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("experience_level",)
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Additional information from the sommelier",
+            {"fields": ("experience_level", "preferred_flavors")},
+        ),
+    )
+    filter_horizontal = ("preferred_flavors",)
+
+
+@admin.register(Beverage)
+class BeverageAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "alcohol_type",
+        "alcohol_strength",
+        "average_price",
+    )
+    search_fields = ("name", "description")
+    list_filter = ("alcohol_type", "flavors")
+    filter_horizontal = ("flavors", "snacks", "tasters")
+
+
+@admin.register(AlcoholType)
+class AlcoholTypeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(Snack)
+class SnackAdmin(admin.ModelAdmin):
+    list_display = ("name", "is_cooking_required")
+    list_filter = ("is_cooking_required",)
+
+
+@admin.register(Flavor)
+class FlavorAdmin(admin.ModelAdmin):
+    list_display = ("name",)
