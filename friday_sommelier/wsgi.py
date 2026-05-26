4e1a2217bf238001fdfa0a7dde1444cf8212ce51
amesemyta1 <amesemyta@gmail.com>
2026-05-26 17:19:15 +0300

    chore: include django project base configuration and settings

--- friday_sommelier/wsgi.py
+++ friday_sommelier/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'friday_sommelier.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "friday_sommelier.settings")
 
 application = get_wsgi_application()
